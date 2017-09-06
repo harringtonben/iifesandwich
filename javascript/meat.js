148c37d64cbec2e4fef04a0e75a8feec308900d6
ben harrington <harringtonben@icloud.com>
2017-09-06 11:40:55 -0500

    started iife

--- javascript/meat.js
+++ javascript/meat.js
@@ -1,1 +1,27 @@
 console.log('meat');
+
+var SandwichBuilder = (function(oldSandwichBuilder) {
+	const meats = ["Turkey", "Ham", "Roast Beef", "Tuna Salad", "I'm an L7 weenie and don't eat meat"];
+	let mySandwich = [];
+	
+	oldSandwichBuilder.getMeats = function() {
+		return meats;
+	};
+
+	oldSandwichBuilder.addToSandwich = function(event) {
+		console.log(event);
+		console.log(event.target.parentNode.children.meat.childNodes[3].checked);
+		console.log(event.target.parentNode.children.meat.childNodes[7].checked);
+		console.log(event.target.parentNode.children.meat.childNodes[11].checked);
+		console.log(event.target.parentNode.children.meat.childNodes[15].checked);
+		console.log(event.target.parentNode.children.meat.childNodes[19].checked);
+		console.log(event.target.parentNode.children.meat.childNodes[3].name);
+		console.log(event.target.parentNode.children.meat.childNodes[7].name);
+		console.log(event.target.parentNode.children.meat.childNodes[11].name);
+		console.log(event.target.parentNode.children.meat.childNodes[15].name);
+		console.log(event.target.parentNode.children.meat.childNodes[19].name);
+	
+	};
+
+	return oldSandwichBuilder;
+})(SandwichBuilder || {});
--- javascript/sandwichmaker.js
+++ javascript/sandwichmaker.js
@@ -1,1 +1,6 @@
 console.log('sandwichmaker');
+
+let submitSandwich = document.getElementById("submit");
+submitSandwich.addEventListener("click", function(event) {
+	SandwichBuilder.addToSandwich(event);
+})
